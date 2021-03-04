2b6b507f811aebd32456ce8693f90e854dd12929
Daniel Pérez Chan <danielpc@mesoamericana.edu.gt>
2021-03-03 19:30:05 -0600

    Se crearon las clases Empleado y Asistencia

---
+++ Laboratorio de repaso 1/Asistencia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_de_repaso_1
+{
+    class Asistencia
+    {
+        int codigo;
+        int horas;
+        string mes;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public int Horas { get => horas; set => horas = value; }
+        public string Mes { get => mes; set => mes = value; }
+    }
+}
---
+++ Laboratorio de repaso 1/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Laboratorio_de_repaso_1
+{
+    class Empleado
+    {
+        int codigo;
+        string nombre;
+        float sueldoHora;
+       // DataTable
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public float SueldoHora { get => sueldoHora; set => sueldoHora = value; }
+    }
+}
