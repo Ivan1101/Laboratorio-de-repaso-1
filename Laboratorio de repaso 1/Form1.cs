4c199fb90e919501cb539033be99266d5fe8d980
Daniel Pérez Chan <danielpc@mesoamericana.edu.gt>
2021-03-04 20:25:30 -0600

    Se realizaron los comentarios en el programa para que este tenga una mejor compresión

--- Laboratorio de repaso 1/Form1.cs
+++ Laboratorio de repaso 1/Form1.cs
@@ -19,10 +19,23 @@ namespace Laboratorio_de_repaso_1
         public Form1()
         {
             InitializeComponent();
+            bloqueo_botones();
+        }
+        void bloqueo_botones()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = false;
+        }
+        void desbloqueo_botones()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
         }
         public void guardar_datos(string archivo) {
+            // Se crea el archivo de texto para cargar los datos
             FileStream stream = new FileStream(archivo, FileMode.OpenOrCreate, FileAccess.Write);
-
             StreamWriter writer = new StreamWriter(stream);
 
             //foreach (var p in empleados)
@@ -31,7 +44,7 @@ namespace Laboratorio_de_repaso_1
             //    writer.WriteLine(p.Nombre);
             //    writer.WriteLine(p.SueldoHora);
             //}
-
+            // Se utiliza el ciclo for para ver el orden de como se van a imprimir los datos en el documento
             for (int x = 0; x < empleados.Count; x++)
             {
                 writer.WriteLine(empleados[x].Codigo);
@@ -42,10 +55,10 @@ namespace Laboratorio_de_repaso_1
             writer.Close();
         }
         public void guardar_datos2(string archivo)
-        {
+        { // Se crea el archivo de texto para cargar los datos y el sueldo mensual
             FileStream stream2 = new FileStream(archivo, FileMode.OpenOrCreate, FileAccess.Write);
             StreamWriter writer2 = new StreamWriter(stream2);
-
+            // Se utiliza el ciclo for para ver el orden de como se van a imprimir los datos en el documento
             for (int x = 0; x < sueldo_mensual.Count; x++)
             {
                 writer2.WriteLine(sueldo_mensual[x].Codigo);
@@ -59,16 +72,16 @@ namespace Laboratorio_de_repaso_1
             FileStream stream = new FileStream("Empleados.txt", FileMode.Open, FileAccess.Read);
             StreamReader reader = new StreamReader(stream);
             while (reader.Peek() > -1)
-            {
+            { // Se instancia la clase cual se va utilizar para agregar los datos de acuerdo a su tipo de dato
                 Empleado persona2Temp = new Empleado();
-
                 persona2Temp.Codigo = Convert.ToInt32(reader.ReadLine());
                 persona2Temp.Nombre = reader.ReadLine();
                 persona2Temp.SueldoHora = float.Parse(reader.ReadLine());
+         // Se añaden los datos a lista para que se queden cargados al archivo de texto y aparezacan al principio cuando se corra el pogra
                 empleados.Add(persona2Temp);
 
             }
-            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+        //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
             reader.Close();
         }
         
@@ -78,20 +91,21 @@ namespace Laboratorio_de_repaso_1
             StreamReader reader2 = new StreamReader(stream2);
             while (reader2.Peek() > -1)
             {
+        // Se instancia la clase cual se va utilizar para agregar los datos de acuerdo a su tipo de dato
                 Sueldo_total personaTemp = new Sueldo_total();
                 personaTemp.Codigo = Convert.ToInt32(reader2.ReadLine());
                 personaTemp.Nombre = reader2.ReadLine();
                 personaTemp.Sueldo_mensual = Convert.ToInt32(reader2.ReadLine());
+         // Se añaden los datos a lista para que se queden cargados al archivo de texto y aparezacan al principio cuando se corra el pogra
                 sueldo_mensual.Add(personaTemp);
             }
-            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+        //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
             reader2.Close();
         }
 
         public void guardar2(string archivo1)
-        {
+        {// Se crea el archivo de texto para cargar la asistencia del empleado
             FileStream stream1 = new FileStream(archivo1, FileMode.OpenOrCreate, FileAccess.Write);
-
             StreamWriter writer1 = new StreamWriter(stream1);
 
             //foreach (var p in asistencias)
@@ -100,7 +114,7 @@ namespace Laboratorio_de_repaso_1
             //    writer.WriteLine(p.Nombre);
             //    writer.WriteLine(p.SueldoHora);
             //}
-
+            // Se utiliza el ciclo for para ver el orden de como se van a imprimir los datos en el documento
             for (int x = 0; x < asistencias.Count; x++)
             {
                 writer1.WriteLine(asistencias[x].Codigo);
@@ -115,30 +129,33 @@ namespace Laboratorio_de_repaso_1
             StreamReader reader1 = new StreamReader(stream1);
             while (reader1.Peek() > -1)
             {
+        // Se instancia la clase cual se va utilizar para agregar los datos de acuerdo a su tipo de dato
                 Asistencia asistenciatemp = new Asistencia();
-
                 asistenciatemp.Codigo = Convert.ToInt32(reader1.ReadLine());
                 asistenciatemp.Horas = Convert.ToInt32(reader1.ReadLine());
                 asistenciatemp.Mes = reader1.ReadLine();
-
+    // Se añaden los datos a lista para que se queden cargados al archivo de texto y aparezacan al principio cuando se corra el pogra
                 asistencias.Add(asistenciatemp);
 
             }
-            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+   //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
             reader1.Close();
         }
         public void Mostrar()
         {
+            // Se muestra el codigo, el nombre y el sueldo por hora del empleado
             dataGridView1_Empleado.DataSource = null;
             dataGridView1_Empleado.DataSource = empleados;
             dataGridView1_Empleado.Refresh();
+        // Se muestra la asistencia del empleado
             dataGridView2_Asistencia.DataSource = null;
             dataGridView2_Asistencia.DataSource = asistencias;
             dataGridView2_Asistencia.Refresh();
         }
         
         public void Mostrar2()
-        {
+        { 
+            // Se muestra el codigo, el nombre y el sueldo mensual del empleado
             dataGridView1_Empleado.DataSource = null;
             dataGridView1_Empleado.DataSource = sueldo_mensual;
             dataGridView1_Empleado.Refresh();
@@ -165,20 +182,25 @@ namespace Laboratorio_de_repaso_1
                 empleadotemp.SueldoHora = Convert.ToInt32(textsueldohora.Text);
                 asistenciatemp.Codigo = Convert.ToInt32(textCodigo.Text);
                 asistenciatemp.Horas = Convert.ToInt32(textBoxHoras.Text);
+                // Operación para obtener el sueldo mensual del empleado
                 empleado_mensual.Sueldo_mensual = empleadotemp.SueldoHora * asistenciatemp.Horas;
                 empleado_mensual.Codigo = empleadotemp.Codigo;
                 empleado_mensual.Nombre = empleadotemp.Nombre;
                 asistenciatemp.Mes = textBoxMes.Text;
+            // Se añaden los campos o datos a la lista
                 empleados.Add(empleadotemp);
                 asistencias.Add(asistenciatemp);
                 sueldo_mensual.Add(empleado_mensual);
+                // Se añaden a los archivos de texto
                 guardar_datos("Empleados.txt");
                 guardar2("Asistencias.txt");
                 guardar_datos2("Empleadostotal.txt");
                 limpiar_campos();
+                desbloqueo_botones();
             }
             catch(Exception )
             {
+                // Condición para emitir si falta en llenar un campo
     MessageBox.Show("No se han llenado todos los datos", "Mi Mesaje Predeterminado", MessageBoxButtons.OK,MessageBoxIcon.Warning);
             }
         }
@@ -192,13 +214,14 @@ namespace Laboratorio_de_repaso_1
         }
 
         private void button2_Click(object sender, EventArgs e)
-        {
+        {  // Botón de mostrar los datos de los empleados
             Mostrar();
             
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            // Botón para mostrar a los empleados con su sueldo mensual y la asistencia
             dataGridView2_Asistencia.DataSource = null;
             dataGridView2_Asistencia.DataSource = asistencias;
             dataGridView2_Asistencia.Refresh();
